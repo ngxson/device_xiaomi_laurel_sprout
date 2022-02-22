914df5229581b895f307a36ca827da2ad3968508
ngxson <thichthat@gmail.com>
2022-02-22 14:13:10 +0100

    add AxetCallRecorder

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -521,9 +521,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Fix Gapps permissions
+# Fix Gapps permissions + Add call recorder
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pix3lify-permissions.xml:$(TARGET_OUT)/system/system_ext/etc/permissions/pix3lify-permissions.xml
+
+PRODUCT_PACKAGES += \
+    AxetCallRecorder
 
 # Inherit the proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
